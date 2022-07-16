09a69b5cc3e4857e5d61baa6c46fb44f1b655cf2
Sahil16Bhatiya <92774669+Sahil16Bhatiya@users.noreply.github.com>
2022-07-15 21:50:41 -0700

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,8 @@
+var myGraph;
+function openGraph() {
+	myGraph = window.open("file:///C:/Users/OMSAI/Desktop/SahilAB/g.png","","width=500px ");
+}
+
+function closeGraph(){
+	myGraph.close();
+}
---
+++ toggleClass.js
@@ -0,0 +1,11 @@
+ function toggleClass()
+    {
+        const body = document.querySelector('body');
+        body.classList.toggle('light');
+    }
+
+    function toggleClass2()
+    {
+        const body = document.querySelector('body');
+        body.classList.toggle('gravity');
+    }
